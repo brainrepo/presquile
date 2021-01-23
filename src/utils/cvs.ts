c361c9d6e0605b29d9733ba7e5b7a16b7389eb5a
brainrepo <murru7@gmail.com>
2021-01-23 23:38:45 +0100

    Fix spacing

--- src/utils/cvs.ts
+++ src/utils/cvs.ts
@@ -7,10 +7,10 @@ const PARSE_ERROR_MESSAGE = 'This is not CVS content'
 /* eslint-disable  @typescript-eslint/no-explicit-any */
 /* eslint-disable  @typescript-eslint/explicit-module-boundary-types */
 export function parse(data:Buffer|string, options:Options):Either<Error, any>{
-    try{
+    try {
         const parsedData = parses(data, options)
-        return (parsedData !== undefined && parsedData.length > 0 ) ? right(parsedData) : left(Error(PARSE_ERROR_MESSAGE))
-    }catch(e){
+        return (parsedData !== undefined && parsedData.length > 0) ? right(parsedData) : left(Error(PARSE_ERROR_MESSAGE))
+    } catch (e){
         return left(Error(PARSE_ERROR_MESSAGE))
     }
 }
