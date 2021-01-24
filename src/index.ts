5e0580090b54de6b074c4bc287256010bf924d0f
brainrepo <murru7@gmail.com>
2021-01-24 17:15:20 +0100

    Create a command-line application

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
+#!/usr/bin/env node
 import { apply } from './drivers/auditionCVS/applier'
+import {Command} from 'commander'
+import { flow } from 'fp-ts/lib/function'
+import { fold } from 'fp-ts/lib/Either'
 
-apply('./mockData/valid_chaps.cvs','./mockData/audio.mp3')()
-    .then(e => console.log(e))
-    .catch(e => console.error(e))
+const program = new Command()
+program.version('1.0.0')
+
+program.command('apply <auditionCVS> <mp3File>')
+    .description('test command', {
+        auditionCVS: 'Audition CVS Markers file',
+        mp3File: 'Mp3 file'
+    })
+    .action(async (auditionCVS, mp3File) => {
+        const r = await apply(auditionCVS, mp3File)()
+
+        flow(
+            fold(
+                (e:Error) => console.error('Error: %s', e.message),
+                () => console.log('Chapters written inside mp3!') 
+            )
+        )(r)
+    })
+
+program.parse(process.argv)
